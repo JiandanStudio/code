9786b0d9fbc4b9ee679bc3fb30a4fb0ad173c774
JiandanStudio <wangshenghua@outlook.com>
2024-01-07 19:11:20 +0800

    new

--- test.py
+++ test.py
@@ -13,11 +13,12 @@ count=0
 i=1
 while(i<10):
     temp = input("猜猜你心里想的什么数字：")
-    guess = int(temp)
-    i = i+1
-    if guess == 8:
-        print("卧槽，你是我心里的蛔虫吗？")
-        break
+    if temp.isdigit() == False:
+        guess = int(temp)
+        i = i+1
+        if guess == 8:
+            print("卧槽，你是我心里的蛔虫吗？")
+            break
 
 for i in range(0,10):
     temp = input("猜猜你心里想的什么数字：")
@@ -26,3 +27,4 @@ for i in range(0,10):
     if guess == 8:
         print("卧槽，你是我心里的蛔虫吗？")
         break
+
